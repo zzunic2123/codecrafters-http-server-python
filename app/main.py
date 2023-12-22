772c98ec7e6ca6ff8d5283f972f3fbfcdb2c7b2b
zzunic <zzunic@playnirvana.com>
2023-12-22 22:59:47 +0100

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -10,8 +10,18 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_connection, client_address = server_socket.accept()
-    client_connection.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+    request_data = client_connection.recv(1204)
+    lines = request_data.decode().split("\r\n")
+    method, path, version = lines[0].split()
 
+    print(lines)
+    response = ""
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    client_connection.send(response.encode())
 
 if __name__ == "__main__":
     main()
