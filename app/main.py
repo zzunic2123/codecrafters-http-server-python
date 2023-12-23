dd193825742a0d8599d0bf771975ec157af39dc2
zzunic <zzunic@playnirvana.com>
2023-12-23 18:26:09 +0100

    pass 7th stage

--- app/main.py
+++ app/main.py
@@ -1,20 +1,12 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+import os
 
 status_codes = {200: 'OK', 404: 'Not Found'}
 
-def build_response(path, status, content_type, header):
-    path = path.split("/")
-    func = path[1]
 
-    body = ""
-    if func == "echo":
-        body = "/".join(path[2:])
-    else:
-        if len(header.split()) >= 2:
-            body = header.split()[1]
-
+def build_response(body, status, content_type):
     response = ("HTTP/1.1 " + str(status) + " " + status_codes[status] + "\r\n"
                 + "Content-Type: " + content_type + "\r\n"
                 + "Content-Length: " + str(len(body)) + "\r\n\r\n"
@@ -22,7 +14,31 @@ def build_response(path, status, content_type, header):
 
     return response
 
-def process_request(client_connection, client_address):
+
+def resolve_body(path, header, directory_path = None):
+
+    path = path.split("/")
+    if not directory_path:
+        func = path[1]
+
+        body = ""
+        if func == "echo":
+            body = "/".join(path[2:])
+        else:
+            if len(header.split()) >= 2:
+                body = header.split()[1]
+
+        return body
+
+    file_path = path[2]
+
+    print(file_path)
+    print(directory_path)
+
+
+
+
+def process_request(client_connection, client_address, directory_path):
     request_data = client_connection.recv(1204)
     lines = request_data.decode().split("\r\n")
     method, path, version = lines[0].split()
@@ -30,20 +46,34 @@ def process_request(client_connection, client_address):
     # header_placeholder, header = lines[2].split()
 
     if path == "/" or path.startswith("/echo/"):
-        response = build_response(path, 200, "text/plain", lines[2])
+        response = build_response(resolve_body(path, lines[2]), 200, "text/plain")
+    elif path.startswith("/files/"):
+        file_path = path.split("/")[2]
+        if not os.path.isfile(directory_path + file_path):
+            response = build_response(resolve_body(path, lines[2]), 404, "text/plain")
+        else:
+            f = open(directory_path + file_path, "r")
+            body = f.read()
+            response = build_response(body, 200, "application/octet-stream")
     else:
-        response = build_response(path, 404, "text/plain", lines[2])
+        response = build_response(resolve_body(path, lines[2]), 404, "text/plain")
 
     client_connection.sendto(response.encode(), client_address)
+
+
+import sys
+
 
 def main():
     print("Logs from your program will appear here!")
 
+    directory_path = ""
+    if len(sys.argv) >= 2:
+        directory_path = sys.argv[2]
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         client_connection, client_address = server_socket.accept()
-        thread = threading.Thread(target=process_request, args=(client_connection,client_address))
-        thread.daemon = True
+        thread = threading.Thread(target=process_request, args=(client_connection, client_address, directory_path))
         thread.start()
 
 
