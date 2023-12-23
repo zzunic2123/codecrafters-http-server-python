d2ef07ec4be479068051dd64a2ab8b7d7b7906b6
zzunic <zzunic@playnirvana.com>
2023-12-23 16:52:56 +0100

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -1,27 +1,34 @@
 # Uncomment this to pass the first stage
 import socket
 
+status_codes = {200: 'OK', 404: 'Not Found'}
+
+
+def build_response(path, status, contentType):
+    path = path.split("/")
+    func = path[0]
+    body = "/".join(path[2:])
+    response = ("HTTP/1.1 " + str(status) + " " + status_codes[status] + "\r\n"
+                + "Content-Type: " + contentType + "\r\n"
+                + "Content-Length: " + str(len(body)) + "\r\n\r\n"
+                + body)
+
+    return response
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_connection, client_address = server_socket.accept()
     request_data = client_connection.recv(1204)
     lines = request_data.decode().split("\r\n")
     method, path, version = lines[0].split()
 
-    print(lines)
-    print(path)
-
-    response = ""
-    if path == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+    if path == "/" or path.startswith("/echo/"):
+        response = build_response(path, 200, "text/plain")
     else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        response = build_response(path, 404, "text/plain")
 
     client_connection.sendall(response.encode())
 
